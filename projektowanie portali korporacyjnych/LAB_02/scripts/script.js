25048fc8d08010a9bb49448f83f74385506cbd70
Jakub <jakub.piotrowski098@gmail.com>
2023-11-17 16:59:07 +0100

    Lab02 format update

--- projektowanie portali korporacyjnych/LAB_02/scripts/script.js
+++ projektowanie portali korporacyjnych/LAB_02/scripts/script.js
@@ -17,6 +17,7 @@ function startKalkulator() {
   document.getElementById("kalkulator").style.display = "initial";
   document.getElementById("operatory").style.display = "none";
   document.getElementById("memory").style.display = "none";
+
   workingOperators();
   allOperators();
 }
@@ -41,6 +42,7 @@ function doKalkulatora() {
   document.getElementById("kalkulator").style.display = "initial";
   document.getElementById("operatory").style.display = "none";
   document.getElementById("memory").style.display = "none";
+
   let selekt = document.getElementById("selectOperator");
   let spanSelektKalkulator = document.getElementById("kalkulatorSelect");
   spanSelektKalkulator.appendChild(selekt);
@@ -50,8 +52,10 @@ function oblicz() {
   let liczba1 = document.getElementById("liczba1").value;
   let liczba2 = document.getElementById("liczba2").value;
   let operator = document.getElementById("selectOperator").value;
+
   let wynik = liczba1 + operator + liczba2;
   wynik = eval(wynik);
+
   if (wynik) {
     document.getElementById("wynik").value = wynik;
     document.getElementById("error").innerHTML = "";
@@ -64,6 +68,7 @@ function oblicz() {
 
 function workingOperators() {
   let selectWorkingOperators = document.getElementById("selectOperator");
+
   for (let operator of operatory) {
     let option = document.createElement("option");
     option.value = operator;
@@ -74,6 +79,7 @@ function workingOperators() {
 
 function allOperators() {
   let selectAddOperator = document.getElementById("selectAddOperator");
+
   for (let operator of operatoryAll) {
     let option = document.createElement("option");
     option.value = operator;
@@ -85,12 +91,14 @@ function allOperators() {
 function dodajOperator() {
   let selectWorkingOperators = document.getElementById("selectOperator");
   let selectAddOperator = document.getElementById("selectAddOperator");
+
   let option = selectAddOperator.options[selectAddOperator.selectedIndex];
   selectWorkingOperators.appendChild(option);
 }
 function usunOperator() {
   let selectWorkingOperators = document.getElementById("selectOperator");
   let selectAddOperator = document.getElementById("selectAddOperator");
+
   let option =
     selectWorkingOperators.options[selectWorkingOperators.selectedIndex];
   selectAddOperator.appendChild(option);
@@ -110,14 +118,17 @@ function wstawWiersz(n1, op, n2, result) {
   let DELETE = document.createElement("td");
   DELETE.innerHTML = "delete";
   let wiersz = document.createElement("tr");
+
   wiersz.appendChild(N1);
   wiersz.appendChild(OP);
   wiersz.appendChild(N2);
   wiersz.appendChild(equal);
   wiersz.appendChild(RES);
   wiersz.appendChild(DELETE);
+
   let tablica = document.getElementById("memoryObliczenia");
   tablica.appendChild(wiersz);
+
   DELETE.onclick = function () {
     this.parentElement.remove();
   };
